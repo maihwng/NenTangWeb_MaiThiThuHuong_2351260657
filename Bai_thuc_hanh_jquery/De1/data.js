1fe79c8f27b542b33d5591f32c3261b09620b04c
maihwng <maithithuhuongxv@gmail.com>
2025-08-10 03:19:49 +0700

    Add files via upload

---
+++ Bai_thuc_hanh_jquery/De1/data.js
@@ -0,0 +1,33 @@
+// data.js
+const employeeData = [
+  {
+    name: "Thomas Hardy",
+    email: "thomashardy@mail.com",
+    address: "89 Charsocuro Rd, Portland, USA",
+    phone: "0171555222",
+  },
+  {
+    name: "Dominique Perrier",
+    email: "dominiqueperrier@mail.com",
+    address: "Oberer Str. 57, Berlin, Germany",
+    phone: "0193857375",
+  },
+  {
+    name: "Maria Anders",
+    email: "mariaanders@mail.com",
+    address: "25, rue Lauriston, Paris, France",
+    phone: "0503559931",
+  },
+  {
+    name: "Fran Wilson",
+    email: "franwilson@mail.com",
+    address: "C/ Araceli, 67, Madrid, Spain",
+    phone: "0346139731",
+  },
+  {
+    name: "Martin Blank",
+    email: "martinblank@mail.com",
+    address: "Via Monte Bianco 34, Turin, Italy",
+    phone: "0401632097",
+  },
+];
